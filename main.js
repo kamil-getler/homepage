107e98b86afd0c751e4656c460b0bcac21630be1
Kamil Getler <kmailgetler@gmail.com>
2020-05-23 00:46:33 +0200

    zmiana naglowka za pomoca js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console. log ('Siema');
+const firstName = `Kamil`;
+const age = 29;
+
+console.log(
+    `Siemanko witam na mojej stronie,nazywan sie ${firstName} i mam ${age} lata.`
+);
+
+const heading = document.querySelector('.head__brown--js');
+
+heading.innerHTML = `Wiadomość wysłna z JS ;)`;

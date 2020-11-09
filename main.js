ae3b679785f0fb839ec04710fc1befebff855ef9
Kamil Getler <kmailgetler@gmail.com>
2020-11-09 18:11:10 +0100

    save

--- main.js
+++ main.js
@@ -6,5 +6,60 @@ console.log(
 );
 
 const heading = document.querySelector('.head__brown--js');
-
+console.log(heading.innerHTML);
 heading.innerHTML = `Witam Cię na stronie wiadomiść wysłąna z pliku main.js;)`;
+console.log(heading.innerHTML);
+
+const deathStart = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target}destroyed`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: `Death Star`,
+    population: 10000,
+    isLightOn: true,
+    commander: {
+        name: 'Darth Vader',
+        age: 44
+    }
+}
+console.log(deathStart);
+console.log(deathStart.commander.name);
+
+
+const stefan = {
+    age: 32,
+    city: 'Warszawa',
+};
+const kamil = {
+    age: stefan.age,
+    city: 'Parysów',
+};
+
+console.log(stefan);
+console.log(kamil);
+
+
+kamil.age = 24;
+
+console.log(kamil);
+
+const button = document.querySelector('.button--js')
+
+console.log(button)
+
+
+button.addEventListener('click', () => {
+    const head = document.querySelector(".change");
+    head.innerHTML = `Woowowooowowoowwoo`;
+    head.classList.toggle('klasa-z-js');
+});
+
+const hamburger = documnet.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('.navigation--open');
+});
